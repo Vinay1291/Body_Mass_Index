147332fbb364bd40eb122d94b5e8449018d175d4
Vinay Vishwakarama <vinayv1291@gmail.com>
2025-11-26 23:51:12 +0530

    First Page Done

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-
-const iconSize = 80.0;
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color(0xFF8D8E98),
-);
+import 'constants.dart';
 
 class IconContent extends StatelessWidget {
   const IconContent({super.key, required this.icon, required this.label});
@@ -17,11 +12,11 @@ class IconContent extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(icon, size: iconSize,),
+        Icon(icon, size: kIconSize,),
         SizedBox(height: 15.0,),
         Text(
           label,
-          style: labelTextStyle,
+          style: kLabelTextStyle,
         ),
       ],
     );
